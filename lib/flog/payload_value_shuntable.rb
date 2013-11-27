15f98d741f27c2cbfd4c2564be484f9ba820d987
pinzolo <pinzolo@gmail.com>
2013-11-27 11:38:00 +0900

    Add test for PayloadValueShuntable

--- lib/flog/payload_value_shuntable.rb
+++ lib/flog/payload_value_shuntable.rb
@@ -1,12 +1,19 @@
 # coding: utf-8
 module PayloadValueShuntable
   def shunt_payload_value(payload, key, temp_value, &block)
+    return unless block
+
+    key_exists = payload.key?(key)
     base_value = payload[key]
     begin
       payload[key] = temp_value
       block.call
     ensure
-      payload[key] = base_value
+      if key_exists
+        payload[key] = base_value
+      else
+        payload.delete(key)
+      end
     end
   end
 end
---
+++ test/unit/payload_value_shuntable_test.rb
@@ -0,0 +1,57 @@
+# coding: utf-8
+require "test_helper"
+
+class TestClass
+  include PayloadValueShuntable
+end
+
+class PayloadValueShuntableTest < ActiveSupport::TestCase
+  def setup
+    @tester = TestClass.new
+    @payload = { foo: "foo_value", bar: "bar_value" }
+  end
+
+  def test_shunt_payload_value
+    @tester.shunt_payload_value(@payload, :foo, "new_value") do
+      assert_equal "new_value", @payload[:foo]
+    end
+  end
+
+  def test_value_of_other_key_is_not_changed
+    @tester.shunt_payload_value(@payload, :foo, "new_value") do
+      assert_equal "bar_value", @payload[:bar]
+    end
+  end
+
+  def test_restoration
+    @tester.shunt_payload_value(@payload, :foo, "new_value") do
+      assert_equal "new_value", @payload[:foo]
+    end
+    assert_equal "foo_value", @payload[:foo]
+  end
+
+  def test_ensure_restoration_on_error_raised_in_block
+    error_raised = false
+    begin
+      @tester.shunt_payload_value(@payload, :foo, "new_value") do
+        raise "error"
+      end
+    rescue => ex
+      error_raised = true
+    end
+    assert error_raised
+    assert_equal "foo_value", @payload[:foo]
+  end
+
+  def test_without_block
+    @tester.shunt_payload_value(@payload, :foo, "new_value")
+    assert_equal "foo_value", @payload[:foo]
+  end
+
+  def test_key_removed_when_give_not_exist_key
+    @tester.shunt_payload_value(@payload, :baz, "new_value") do
+      assert_equal "new_value", @payload[:baz]
+    end
+    assert_equal false, @payload.key?(:baz)
+  end
+end
