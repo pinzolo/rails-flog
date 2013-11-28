333eb0a74e551c8196c978c10842654583cd60d4
pinzolo <pinzolo@gmail.com>
2013-11-28 10:44:39 +0900

    Add filter for coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 # coding: utf-8
 require "coveralls"
-Coveralls.wear!
+require "simplecov"
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/bundle/'
+end
 
 Bundler.require
 require "flog"
