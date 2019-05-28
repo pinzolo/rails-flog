b837d8b03129b0b700309120a75c2b3f2f3f5e7d
pinzolo <pinzolo@gmail.com>
2019-05-28 22:55:09 +0900

    Solve Lint/IneffectiveAccessModifier cop

--- lib/flog/status.rb
+++ lib/flog/status.rb
@@ -5,41 +5,42 @@ module Flog
     SWITCH_FILE_NAME = 'no-flog.txt'
     SQL_SWITCH_FILE_NAME = 'no-flog-sql.txt'
     PARAMS_SWITCH_FILE_NAME = 'no-flog-params.txt'
-
-    def self.enabled?
-      !switch_file_exists?(SWITCH_FILE_NAME)
-    rescue
-      true
-    end
-
-    def self.sql_formattable?
-      enabled? && !switch_file_exists?(SQL_SWITCH_FILE_NAME)
-    rescue
-      true
-    end
-
-    def self.params_formattable?
-      enabled? && !switch_file_exists?(PARAMS_SWITCH_FILE_NAME)
-    rescue
-      true
-    end
-
-    def self.switch_file_base_path
-      if Rails.root && Rails.root.exist?
-        Rails.root
-      else
-        Pathname.new(File.expand_path(File.dirname(__FILE__) + '../../'))
+    class << self
+      def enabled?
+        !switch_file_exists?(SWITCH_FILE_NAME)
+      rescue
+        true
       end
-    end
 
-    def self.switch_file_dir_path
-      switch_file_base_path.join('tmp')
-    end
+      def sql_formattable?
+        enabled? && !switch_file_exists?(SQL_SWITCH_FILE_NAME)
+      rescue
+        true
+      end
 
-    private
+      def params_formattable?
+        enabled? && !switch_file_exists?(PARAMS_SWITCH_FILE_NAME)
+      rescue
+        true
+      end
 
-    def self.switch_file_exists?(file_name)
-      switch_file_dir_path.join(file_name).exist?
+      def switch_file_base_path
+        if Rails.root && Rails.root.exist?
+          Rails.root
+        else
+          Pathname.new(File.expand_path(File.dirname(__FILE__) + '../../'))
+        end
+      end
+
+      def switch_file_dir_path
+        switch_file_base_path.join('tmp')
+      end
+
+      private
+
+      def switch_file_exists?(file_name)
+        switch_file_dir_path.join(file_name).exist?
+      end
     end
   end
 end
