c8e3e9dd41bf9f90d3f12a29c732d5a68fefbdc3
pinzolo <pinzolo@gmail.com>
2019-05-28 23:01:10 +0900

    Solve Style/ClassAndModuleChildren cop

--- lib/flog/params_formattable.rb
+++ lib/flog/params_formattable.rb
@@ -2,61 +2,65 @@ require 'action_controller/log_subscriber'
 require 'awesome_print'
 require 'flog/payload_value_shuntable'
 
-module Flog::ParamsFormattable
-  include Flog::PayloadValueShuntable
-  def start_processing(event)
-    return super(event) unless formattable?(event)
+module Flog
+  module ParamsFormattable
+    include Flog::PayloadValueShuntable
+    def start_processing(event)
+      return super(event) unless formattable?(event)
 
-    replaced = replace_params(event.payload[:params])
+      replaced = replace_params(event.payload[:params])
 
-    shunt_payload_value(event.payload, :params, replaced) do
-      super(event)
-    end
-  end
-
-  private
-
-  def replace_params(params)
-    return params if params.empty? || !params.respond_to?(:ai)
-
-    replaced = params.dup
-    class << replaced
-      alias_method :original_except, :except
-
-      def except(*keys)
-        excepted = original_except(*keys)
-        class << excepted
-          def inspect
-            "\n#{ai(plain: !ActionController::LogSubscriber.colorize_logging)}"
-          end
-        end
-        excepted
+      shunt_payload_value(event.payload, :params, replaced) do
+        super(event)
       end
     end
-    replaced
-  end
 
-  def formattable?(event)
-    return false unless Flog::Status.params_formattable?
+    private
 
-    return true if force_format_by_nested_params?(event)
+    def replace_params(params)
+      return params if params.empty? || !params.respond_to?(:ai)
 
-    key_count_over?(event)
-  end
+      replaced = params.dup
+      class << replaced
+        alias_method :original_except, :except
 
-  def force_format_by_nested_params?(event)
-    return false unless Flog.config.force_on_nested_params?
+        def except(*keys)
+          excepted = original_except(*keys)
+          class << excepted
+            def inspect
+              "\n#{ai(plain: !ActionController::LogSubscriber.colorize_logging)}"
+            end
+          end
+          excepted
+        end
+      end
+      replaced
+    end
 
-    event.payload[:params].values.any? { |value| value.is_a?(Hash) }
-  end
+    def formattable?(event)
+      return false unless Flog::Status.params_formattable?
 
-  def key_count_over?(event)
-    threshold = Flog.config.params_key_count_threshold.to_i
-    params = event.payload[:params].except(*ActionController::LogSubscriber::INTERNAL_PARAMS)
-    params.keys.size > threshold
+      return true if force_format_by_nested_params?(event)
+
+      key_count_over?(event)
+    end
+
+    def force_format_by_nested_params?(event)
+      return false unless Flog.config.force_on_nested_params?
+
+      event.payload[:params].values.any? { |value| value.is_a?(Hash) }
+    end
+
+    def key_count_over?(event)
+      threshold = Flog.config.params_key_count_threshold.to_i
+      params = event.payload[:params].except(*ActionController::LogSubscriber::INTERNAL_PARAMS)
+      params.keys.size > threshold
+    end
   end
 end
 
-class ActionController::LogSubscriber
-  prepend Flog::ParamsFormattable
+module ActionController
+  class LogSubscriber
+    prepend Flog::ParamsFormattable
+  end
 end
--- lib/flog/sql_formattable.rb
+++ lib/flog/sql_formattable.rb
@@ -1,52 +1,56 @@
 require 'active_record/log_subscriber'
 require 'flog/payload_value_shuntable'
 
-module Flog::SqlFormattable
-  include Flog::PayloadValueShuntable
+module Flog
+  module SqlFormattable
+    include Flog::PayloadValueShuntable
 
-  def sql(event)
-    return super(event) unless formattable?(event)
+    def sql(event)
+      return super(event) unless formattable?(event)
 
-    formatted = format_sql(event.payload[:sql])
+      formatted = format_sql(event.payload[:sql])
 
-    shunt_payload_value(event.payload, :sql, "\n#{Flog.config.sql_indent}#{formatted}") do
-      super(event)
+      shunt_payload_value(event.payload, :sql, "\n#{Flog.config.sql_indent}#{formatted}") do
+        super(event)
+      end
     end
-  end
 
-  private
+    private
 
-  def format_sql(sql)
-    return sql if sql.blank?
+    def format_sql(sql)
+      return sql if sql.blank?
 
-    require 'anbt-sql-formatter/formatter'
-    rule = AnbtSql::Rule.new
-    rule.keyword = AnbtSql::Rule::KEYWORD_UPPER_CASE
-    rule.indent_string = Flog.config.sql_indent
-    rule.in_values_num = Flog.config.sql_in_values_num
-    %w(count sum).each do |function_name|
-      rule.function_names << function_name
+      require 'anbt-sql-formatter/formatter'
+      rule = AnbtSql::Rule.new
+      rule.keyword = AnbtSql::Rule::KEYWORD_UPPER_CASE
+      rule.indent_string = Flog.config.sql_indent
+      rule.in_values_num = Flog.config.sql_in_values_num
+      %w(count sum).each do |function_name|
+        rule.function_names << function_name
+      end
+      AnbtSql::Formatter.new(rule).format(sql.squeeze(' '))
     end
-    AnbtSql::Formatter.new(rule).format(sql.squeeze(' '))
-  end
 
-  def formattable?(event)
-    return false unless Flog::Status.sql_formattable?
+    def formattable?(event)
+      return false unless Flog::Status.sql_formattable?
 
-    return false if ignore_by_cached_query?(event)
+      return false if ignore_by_cached_query?(event)
 
-    duration_over?(event)
-  end
+      duration_over?(event)
+    end
 
-  def ignore_by_cached_query?(event)
-    (event.payload[:name] == 'CACHE' || event.payload[:cached]) && Flog.config.ignore_cached_query?
-  end
+    def ignore_by_cached_query?(event)
+      (event.payload[:name] == 'CACHE' || event.payload[:cached]) && Flog.config.ignore_cached_query?
+    end
 
-  def duration_over?(event)
-    event.duration >= Flog.config.query_duration_threshold.to_f
+    def duration_over?(event)
+      event.duration >= Flog.config.query_duration_threshold.to_f
+    end
   end
 end
 
-class ActiveRecord::LogSubscriber
-  prepend Flog::SqlFormattable
+module ActiveRecord
+  class LogSubscriber
+    prepend Flog::SqlFormattable
+  end
 end
